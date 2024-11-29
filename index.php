a2e4a55df20896add9e4321b21385639e7930fc4
denayprm <152400121+denayprm@users.noreply.github.com>
2024-11-30 00:40:44 +0700

    feat: index

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php include "layout/header.html" ?>
+
+    <main>
+        <p>Hallo Selamat Datang di Website Latihan</p>
+    </main>
+
+    <?php include "layout/footer.html" ?>
+</body>
+
+</html>
